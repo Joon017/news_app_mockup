9ccba7ea5f32219b449b4c7d2bdb01a7c10be8b6
Joon Xiang Yong <jxyong.2021@scis.smu.edu.sg>
2023-12-28 09:25:31 +0800

    Added path for vision branch

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def homepage():
     return render_template("index.html")
 
-
-
+@app.route('/vision')
+def gpt_vision():
+    return render_template("vision.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
